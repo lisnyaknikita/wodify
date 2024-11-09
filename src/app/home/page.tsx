7594f1fafbc0cc82ce6a34ee84237158b73d878e
nlisnyak <lisnyaknikita@gmail.com>
2024-11-09 15:06:42 +0200

    init: branc for home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,22 +1,9 @@
-'use client'
-
-import { useCurrentUser } from '@/features/auth/hooks/use-current-user'
-
-import { useAuthActions } from '@convex-dev/auth/react'
-
-import { Loader } from 'lucide-react'
-
 export default function HomePage() {
-	const { signOut } = useAuthActions()
-
-	const { data, isLoading } = useCurrentUser()
-
-	if (isLoading) return <Loader />
-
 	return (
 		<div>
-			Hello, {data?.name}
-			<button onClick={() => void signOut()}>Logout</button>
+			{/* <Calendar/>
+			<LastTrainingSession/>
+			<LastNote/> */}
 		</div>
 	)
 }
