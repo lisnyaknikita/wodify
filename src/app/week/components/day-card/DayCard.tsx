a291aa08e22a6a8cb58c7bb5b59889156d6758c7
nlisnyak <lisnyaknikita@gmail.com>
2024-12-04 22:53:48 +0200

    feat: displayed session's plan

--- src/app/week/components/day-card/DayCard.tsx
+++ src/app/week/components/day-card/DayCard.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link'
 
 import { IWeekDay } from '@/shared/helpers/getCurrentWeek'
+import { ISession } from '@/shared/types/session'
+
+import { calculatePlanSummary } from '@/shared/helpers/calculatePlanSummary'
 
 import { format, parseISO } from 'date-fns'
 
-import { ISession } from '@/shared/types/session'
 import classes from './DayCard.module.scss'
 
 interface IDayCardProps {
@@ -21,6 +23,12 @@ export const DayCard = ({ day, sessions }: IDayCardProps) => {
 		return acc
 	}, {})
 
+	const sessionForDay = sessions.find(session => session.date === day.date)
+
+	const planSummary = sessionForDay
+		? calculatePlanSummary(sessionForDay.plan)
+		: { totalExercises: 0, totalSets: 0, totalReps: 0 }
+
 	return (
 		<li className={`${classes.day} ${day.date === today ? classes.currentDay : ''}`}>
 			<div className={classes.date}>
@@ -29,9 +37,9 @@ export const DayCard = ({ day, sessions }: IDayCardProps) => {
 			<h3 className={classes.name}>{sessionsByDate[day.date] || 'No session'}</h3>
 			<div className={classes.plan}>
 				<h6 className={classes.planLabel}>Plan:</h6>
-				<span className={classes.planRow}>4 exercises</span>
-				<span className={classes.planRow}>12 sets</span>
-				<span className={classes.planRow}>144 repetitions</span>
+				<span className={classes.planRow}>{planSummary.totalExercises} exercises</span>
+				<span className={classes.planRow}>{planSummary.totalSets} sets</span>
+				<span className={classes.planRow}>{planSummary.totalReps} repetitions</span>
 			</div>
 			<Link className={classes.noteLink} href={'/note'}>
 				Note
---
+++ src/shared/helpers/calculatePlanSummary.ts
@@ -0,0 +1,7 @@
+export const calculatePlanSummary = (plan: { exercise: string; sets: number; reps: number }[]) => {
+	const totalExercises = plan.length
+	const totalSets = plan.reduce((sum, item) => sum + item.sets, 0)
+	const totalReps = plan.reduce((sum, item) => sum + item.reps, 0)
+
+	return { totalExercises, totalSets, totalReps }
+}
