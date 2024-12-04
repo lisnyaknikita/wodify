6bf94b4136af6ccc9b082b9c44492ee63a771709
nlisnyak <lisnyaknikita@gmail.com>
2024-12-04 22:36:58 +0200

    feat: implemented calendar view with real date and real training sessions

--- src/app/week/components/day-card/DayCard.tsx
+++ src/app/week/components/day-card/DayCard.tsx
@@ -1,14 +1,32 @@
 import Link from 'next/link'
 
+import { IWeekDay } from '@/shared/helpers/getCurrentWeek'
+
+import { format, parseISO } from 'date-fns'
+
+import { ISession } from '@/shared/types/session'
 import classes from './DayCard.module.scss'
 
-export const DayCard = () => {
+interface IDayCardProps {
+	day: IWeekDay
+	sessions: ISession[]
+}
+
+export const DayCard = ({ day, sessions }: IDayCardProps) => {
+	const today = format(new Date(), 'yyyy-MM-dd')
+
+	const sessionsByDate: Record<string, string> = sessions?.reduce((acc, session) => {
+		//@ts-expect-error ...
+		acc[session.date] = session.title
+		return acc
+	}, {})
+
 	return (
-		<li className={classes.day}>
+		<li className={`${classes.day} ${day.date === today ? classes.currentDay : ''}`}>
 			<div className={classes.date}>
-				November, 06 <br /> Wednesday
+				{format(parseISO(day.date), 'MMMM, dd')} <br /> {day.day}
 			</div>
-			<h3 className={classes.name}>Legs</h3>
+			<h3 className={classes.name}>{sessionsByDate[day.date] || 'No session'}</h3>
 			<div className={classes.plan}>
 				<h6 className={classes.planLabel}>Plan:</h6>
 				<span className={classes.planRow}>4 exercises</span>
--- src/app/week/components/week-board/WeekBoard.tsx
+++ src/app/week/components/week-board/WeekBoard.tsx
@@ -1,18 +1,42 @@
+'use client'
+
+import { getCurrentWeek } from '@/shared/helpers/getCurrentWeek'
+import { useGetSessionsByWeek } from '@/shared/hooks/useGetSessionsByWeek'
+
 import { DayCard } from '../day-card/DayCard'
+
+import { Loader } from 'lucide-react'
 
 import classes from './WeekBoard.module.scss'
 
 export const WeekBoard = () => {
+	const currentWeek = getCurrentWeek()
+	const startDate = currentWeek[0].date
+	const endDate = currentWeek[currentWeek.length - 1].date
+	const { data: sessions, isLoading } = useGetSessionsByWeek({ startDate, endDate })
+
+	if (isLoading) {
+		return (
+			<div className={classes.weekBoard}>
+				<Loader className='loader' />
+			</div>
+		)
+	}
+
+	if (!sessions) {
+		return (
+			<div className={classes.weekBoard}>
+				<p>Error</p>
+			</div>
+		)
+	}
+
 	return (
 		<div className={classes.weekBoard}>
 			<ul className={classes.days}>
-				<DayCard />
-				<DayCard />
-				<DayCard />
-				<DayCard />
-				<DayCard />
-				<DayCard />
-				<DayCard />
+				{currentWeek.map(day => (
+					<DayCard key={day.date} day={day} sessions={sessions} />
+				))}
 			</ul>
 		</div>
 	)
